ddf21dd9f2a0ba5777b0952f3bf4b83a09ad8ada
AhElb <ahmedelbaradey2@gmail.com>
2023-11-25 14:16:17 +0200

    Delete lib directory

--- lib/ui/screen/registration/PLOH/cubit/auth_state.dart
+++
@@ -1,8 +0,0 @@
-abstract class AuthStates {}
-class AuthInitialState extends AuthStates{}
-class AuthSuccessState extends AuthStates{}
-class AuthErrorState extends AuthStates{
-  String message;
-  AuthErrorState({required this.message});
-}
-class AuthLoadingState extends AuthStates{}
