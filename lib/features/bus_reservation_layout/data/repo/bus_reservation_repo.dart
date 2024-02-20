dace5f7fd46100f206421c65d8893138b35bdf59
Maher El-Amary <maherelamary@Mahers-MacBook-Pro.local>
2024-02-20 03:21:18 -0800

    ###

--- lib/features/bus_reservation_layout/data/repo/bus_reservation_repo.dart
+++ lib/features/bus_reservation_layout/data/repo/bus_reservation_repo.dart
@@ -1,22 +1,17 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:swa/config/routes/app_routes.dart';
 import 'package:swa/core/api/end_points.dart';
-import 'package:swa/features/bus_reservation_layout/data/models/Reservation_Response_fawry_model.dart';
 
 import '../../../../core/api/api_consumer.dart';
-import '../../../payment/select_payment/data/models/payment_message_response_model.dart';
 import '../models/BusSeatsModel.dart';
-import '../models/reservation_request_model.dart';
 
 class BusLayoutRepo {
   final ApiConsumer apiConsumer;
   BusLayoutRepo({required this.apiConsumer});
   Future<BusSeatsModel> getBusSeatsData({required int tripId}) async {
-    var response = await apiConsumer.get(
-    "${EndPoints.baseUrl}Trip/GetSingleTripDetails?tripId=$tripId"
-    );
+    var response = await apiConsumer
+        .get("${EndPoints.baseUrl}Trip/GetSingleTripDetails?tripId=$tripId");
 
     log('ReservationData response ' + response.body);
 
@@ -24,6 +19,4 @@ class BusLayoutRepo {
     BusSeatsModel busSeatsModel = BusSeatsModel.fromJson(decodedResponse);
     return busSeatsModel;
   }
-
-
 }
